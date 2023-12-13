e6b111425bdbc7ad1d7ddb1c0e9901aa0d896424
Sairajud2001 <112760006+Sairajud2001@users.noreply.github.com>
2023-12-13 11:47:38 +0530

    Create 1582. Special Positions in a Binary Matrix brute force approach.py

---
+++ 1582. Special Positions in a Binary Matrix brute force approach.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        count=0
+        total=0
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j]==1:
+                    count+=1
+                    a=0
+                    b=0
+                    while(b<len(mat[0])):
+                        if mat[i][b]==1 and b!=j:
+                            count+=1
+                        b+=1
+                    while(a<len(mat)):
+                        if mat[a][j]==1 and a!=i:
+                            count+=1
+                        a+=1
+                if count==1:
+                    total+=1
+                count=0
+        return total
